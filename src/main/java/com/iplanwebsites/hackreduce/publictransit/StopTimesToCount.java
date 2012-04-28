6328c48441617d05e763d9ca3636ae8f53ce1ebf
cesine <cesine@yahoo.com>
2012-04-28 13:40:04 -0400

    dont need two files each

--- src/main/java/com/iplanwebsites/hackreduce/publictransit/StopTimesToCount.java
+++ src/main/java/com/iplanwebsites/hackreduce/publictransit/StopTimesToCount.java
@@ -1,67 +1,111 @@
 package com.iplanwebsites.hackreduce.publictransit;
 
 import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import org.hackreduce.mappers.StockExchangeMapper;
-import org.hackreduce.models.StockExchangeRecord;
 
 
 /**
- * This MapReduce job will read the NASDAQ or NYSE daily prices dataset and output the highest market caps
- * obtained by each Stock symbol.
+ * This MapReduce job will count the total number of NASDAQ or NYSE records stored within the
+ * files of the given input directories. It will also skip the CSV header.
+ *
+ * It's meant to be an explicit example to show all the moving parts of a MapReduce job. Much of
+ * the code is just copied from the models and mapper package.
  *
  */
 public class StopTimesToCount extends Configured implements Tool {
 
 	public enum Count {
-		STOCK_SYMBOLS
+		RECORDS_SKIPPED,
+		TOTAL_KEYS,
+		UNIQUE_KEYS
 	}
 
-	public static class MarketCapitalizationMapper extends StockExchangeMapper<Text, DoubleWritable> {
+	public static class RecordCounterMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+		// Our own made up key to send all counts to a single Reducer, so we can
+		// aggregate a total value.
+		public static final Text TOTAL_COUNT = new Text("total");
+
+		// Just to save on object instantiation
+		public static final LongWritable ONE_COUNT = new LongWritable(1);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
 		@Override
-		protected void map(StockExchangeRecord record, Context context) throws IOException, InterruptedException {
-			double marketCap = record.getStockPriceClose() * record.getStockVolume();
-			context.write(new Text(record.getStockSymbol()), new DoubleWritable(marketCap));
-		}
+		@SuppressWarnings("unused")
+		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String inputString = value.toString();
 
-	}
+			try {
+				// This code is copied from the constructor of StockExchangeRecord
 
-	public static class MarketCapitalizationReducer extends Reducer<Text, DoubleWritable, Text, Text> {
+				String[] attributes = inputString.split(",");
 
-		NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault());
+				if (attributes.length != 9)
+					throw new IllegalArgumentException("Input string given did not have 9 values in CSV format");
 
-		@Override
-		protected void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-			context.getCounter(Count.STOCK_SYMBOLS).increment(1);
-
-			double highestCap = 0.0;
-			for (DoubleWritable value : values) {
-				highestCap = Math.max(highestCap, value.get());
+				try {
+					String exchange = attributes[0];
+					String stockSymbol = attributes[1];
+					Date date = sdf.parse(attributes[2]);
+					double stockPriceOpen = Double.parseDouble(attributes[3]);
+					double stockPriceHigh = Double.parseDouble(attributes[4]);
+					double stockPriceLow = Double.parseDouble(attributes[5]);
+					double stockPriceClose = Double.parseDouble(attributes[6]);
+					int stockVolume = Integer.parseInt(attributes[7]);
+					double stockPriceAdjClose = Double.parseDouble(attributes[8]);
+				} catch (ParseException e) {
+					throw new IllegalArgumentException("Input string contained an unknown value that couldn't be parsed");
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException("Input string contained an unknown number value that couldn't be parsed");
+				}
+			} catch (Exception e) {
+				context.getCounter(Count.RECORDS_SKIPPED).increment(1);
+				return;
 			}
 
-			context.write(key, new Text(currencyFormat.format(highestCap)));
+			context.getCounter(Count.TOTAL_KEYS).increment(1);
+			context.write(TOTAL_COUNT, ONE_COUNT);
 		}
 
 	}
 
+	public static class RecordCounterReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+		@Override
+		protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+			context.getCounter(Count.UNIQUE_KEYS).increment(1);
+
+			long count = 0;
+			for (LongWritable value : values) {
+				count += value.get();
+			}
+
+			context.write(key, new LongWritable(count));
+		}
+
+	}
+	
 	@Override
 	public int run(String[] args) throws Exception {
-        Configuration conf = getConf();
+		Configuration conf = getConf();
 
         if (args.length != 2) {
         	System.err.println("Usage: " + getClass().getName() + " <input> <output>");
@@ -74,19 +118,20 @@ public class StopTimesToCount extends Configured implements Tool {
         job.setJobName(getClass().getName());
 
         // Tell the job which Mapper and Reducer to use (classes defined above)
-        job.setMapperClass(MarketCapitalizationMapper.class);
-		job.setReducerClass(MarketCapitalizationReducer.class);
+        job.setMapperClass(RecordCounterMapper.class);
+		job.setReducerClass(RecordCounterReducer.class);
 
-		// Configure the job to accept the NASDAQ/NYSE data as input
-		StockExchangeMapper.configureJob(job);
+		// The Nasdaq/NYSE data dumps comes in as a CSV file (text input), so we configure
+		// the job to use this format.
+		job.setInputFormatClass(TextInputFormat.class);
 
 		// This is what the Mapper will be outputting to the Reducer
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(DoubleWritable.class);
+		job.setMapOutputValueClass(LongWritable.class);
 
 		// This is what the Reducer will be outputting
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
 
 		// Setting the input folder of the job 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
