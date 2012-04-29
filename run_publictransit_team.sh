367a4910187b3a4d2b1ae3ba9945b15f50ae3b07
cesine <cesine@yahoo.com>
2012-04-28 20:15:32 -0400

    shell script to run locally all the jobs

--- run_publictransit_team.sh
+++ run_publictransit_team.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs /tmp/gtfs_stoptimecounts
-java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash /tmp/gtfs_stoptimecounts datasets/gtfs/torontogo/stops.txt /tmp/gtfs_geohash
+java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/san-fran/stop_times.txt output/gtfs_stoptimecounts/san-fran
+java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/torontogo/stop_times.txt output/gtfs_stoptimecounts/toronto
+java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/washinton/stop_times.txt output/gtfs_stoptimecounts/washington
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/san-fran dataset/gtfs/san-fran/stops.txt output/gtfs_geohash/san-fran
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/torontogo dataset/gtfs/torontogo/stops.txt output/gtfs_geohash/torontogo
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/washington dataset/gtfs/washington/stops.txt output/gtfs_geohash/washington
 
