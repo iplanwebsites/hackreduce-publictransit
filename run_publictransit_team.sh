900551e37c72c855b7b810599b56d37e1e0c13d9
cesine <cesine@yahoo.com>
2012-04-28 21:33:22 -0400

    fixes #2 script to locally run all jobs and glue output together is ready

--- run_publictransit_team.sh
+++ run_publictransit_team.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+rm -rf output
 mkdir output/gtfs_stoptimecounts/washington
 mkdir output/gtfs_stoptimecounts/san-fran
 mkdir output/gtfs_stoptimecounts/torontogo
+mkdir output/gtfs_geohash/washington
+mkdir output/gtfs_geohash/torontogo
+mkdir output/gtfs_geohash/san-fran
+mkdir output/final
 
-java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/san-fran/stop_times.txt output/gtfs_stoptimecounts/san-fran
-java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/torontogo/stop_times.txt output/gtfs_stoptimecounts/toronto
-java -Xms1000m -Xmx1000m -classpath ".;build/libs/hackreduce-publictransit-0.2.jar;lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/washinton/stop_times.txt output/gtfs_stoptimecounts/washington
+
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/san-fran/stop_times.txt output/gtfs_stoptimecounts/san-fran/
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/torontogo/stop_times.txt output/gtfs_stoptimecounts/torontogo/
+java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesToCount datasets/gtfs/washington/stop_times.txt output/gtfs_stoptimecounts/washington/
 java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/san-fran datasets/gtfs/san-fran/stops.txt output/gtfs_geohash/san-fran
 java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/torontogo datasets/gtfs/torontogo/stops.txt output/gtfs_geohash/torontogo
 java -Xms1000m -Xmx1000m -classpath ".:build/libs/hackreduce-publictransit-0.2.jar:lib/*" com.iplanwebsites.hackreduce.publictransit.StopTimesCountToGeoHash output/gtfs_stoptimecounts/washington datasets/gtfs/washington/stops.txt output/gtfs_geohash/washington
 
+
+cat output/gtfs_geohash/washington/* >> output/final/washington.json
+cat output/gtfs_geohash/san-fran/* >> output/final/san-fran.json
+cat output/gtfs_geohash/torontogo/* >> output/final/toronto.json
